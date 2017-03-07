e0c2cc589708bca215f99363c6c8764612c9db30
Yang <yuanyang3335@163.com>
2017-03-06 23:47:26 -0800

    Enhance several parts of add transaction view(segue to same view for expense/income, cancel/done button go back to home page, text field keyboard dismiss, datePicker maximum date, and small UI changes)

--- AccountBook/HomeViewController.swift
+++ AccountBook/HomeViewController.swift
@@ -9,20 +9,43 @@
 import UIKit
 
 class HomeViewController: UIViewController, UIPopoverPresentationControllerDelegate {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     // MARK: - Navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if let targetViewController = segue.destination.contents as? NewTransactionViewController {
+                switch identifier {
+                case "Add Expense":
+                    targetViewController.transactionType = "Expense"
+                    targetViewController.navigationItem.title = "Add New Expense"
+                default:
+                    targetViewController.transactionType = "Income"
+                    targetViewController.navigationItem.title = "Add New Income"
+                }
+            }
+        }
+    }
+    
+}
 
+// Cite: - from lecture
+extension UIViewController {
+    var contents: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
 }
---
+++ AccountBook/NewTransactionViewController.swift
@@ -0,0 +1,104 @@
+//
+//  NewTransactionViewController.swift
+//  AccountBook
+//
+//  Created by yang on 3/6/17.
+//  Copyright © 2017 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class NewTransactionViewController: UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    @IBOutlet weak var categoryPicker: UIPickerView!
+    
+    @IBOutlet weak var commentTextField: UITextField!
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        // TODO: Add confirmation message
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func submit(_ sender: UIBarButtonItem) {
+        // TODO: Add constraint check and core data part
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    var transactionType: String?
+    
+    override func viewDidLoad() {
+        amountTextField.delegate = self
+        commentTextField.delegate = self
+        datePicker.maximumDate = Date()
+    }
+    
+    // MARK: - PickerView Data Source and Delegate
+    private let expenseCategories = ["Necessaries", "Shopping", "Transportation", "Fee", "Others"]
+    
+    private let incomeCategories = ["Salary", "Finance", "Others"]
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if transactionType == nil {
+            return 0
+        } else if transactionType! == "Expense" {
+            return expenseCategories.count
+        } else {
+            return incomeCategories.count
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if transactionType == nil {
+            return nil
+        } else if transactionType! == "Expense" {
+            return expenseCategories[row]
+        } else {
+            return incomeCategories[row]
+        }
+    }
+    
+    // MARK: - dismiss keyboard for text field
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
