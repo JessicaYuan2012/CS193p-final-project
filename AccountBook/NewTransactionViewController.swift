e4d4e7ffbbead04c6ab9663d457dca2a14ad6797
Yang <yuanyang3335@163.com>
2017-03-10 17:09:07 -0800

    core plot installed

--- AccountBook/NewTransactionViewController.swift
+++ AccountBook/NewTransactionViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 // CITE: https://makeapppie.com/2016/06/28/how-to-use-uiimagepickercontroller-for-a-camera-and-photo-library-in-swift-3-0/
 class NewTransactionViewController: UITableViewController, UINavigationControllerDelegate {
     @IBOutlet weak var doneButton: UIBarButtonItem!
